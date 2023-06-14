fa9c656a839c4352dcd0186f68a313eeb3660a0b
David182003 <108392777+David182003@users.noreply.github.com>
2023-06-14 14:51:33 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+window.onload = function() {
+
+
 function showTime() {
     const clock = document.getElementById("clock");
     const date = new Date();
@@ -11,3 +14,46 @@ function showTime() {
     clock.textContent = `${formattedHours}:${formattedMinutes}:${formattedSeconds} ${ampm}`;
     setTimeout(showTime, 1000); // actualizar cada segundo
   }
+/*
+ showTime()
+ function actualizar() {
+  var contador = parseInt(localStorage.getItem("contador")); // Obtener el valor actual del contador desde el almacenamiento local
+
+  if (isNaN(contador)) {
+    contador = 0; // Si el contador no existe o no es un número, iniciar en 0
+  }
+
+  contador++; // Incrementar el contador en 1
+
+  localStorage.setItem("contador", contador); // Guardar el nuevo valor del contador en el almacenamiento local
+
+  document.getElementById("contador").textContent = contador; // Mostrar la cantidad de actualizaciones en el elemento con id "contador"
+}
+actualizar()*/
+
+function actualizar() {
+  var contador = parseInt(localStorage.getItem("contador")); // Obtener el valor actual del contador desde el almacenamiento local
+
+  if (isNaN(contador)) {
+    contador = 0; // Si el contador no existe o no es un número, iniciar en 0
+  }
+
+  contador++; // Incrementar el contador en 1
+
+  localStorage.setItem("contador", contador); // Guardar el nuevo valor del contador en el almacenamiento local
+
+  document.getElementById("contador").textContent = contador; // Mostrar la cantidad de visitas en el elemento con id "contador"
+}
+
+
+const boton = document.getElementById('boton');
+const contador = document.getElementById('contador');
+
+boton.addEventListener('click', () => {
+  let cantidadActual = parseInt(contador.innerText);
+  if (cantidadActual > 0) {
+    cantidadActual--;
+    contador.innerText = cantidadActual;
+  }
+});
+};
