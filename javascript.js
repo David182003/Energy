60538f26c82a484644896db7a7ff7fdad723287c
David182003 <108392777+David182003@users.noreply.github.com>
2023-06-14 13:06:28 -0500

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function showTime() {
+    const clock = document.getElementById("clock");
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const ampm = hours >= 12 ? "PM" : "AM";
+    const formattedHours = hours % 12 || 12;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    clock.textContent = `${formattedHours}:${formattedMinutes}:${formattedSeconds} ${ampm}`;
+    setTimeout(showTime, 1000); // actualizar cada segundo
+  }
